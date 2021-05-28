253d45c23760f684a904059947c7b494adb0e3c9
Alberto Marianno de Santana Santos <alberto-marianno.santos@serpro.gov.br>
2021-05-28 10:22:22 -0300

    sketch inicial

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # keba8
 - 👋 Hi, I’m @amarianno
-- 👀 I’m interested in ...
+- 👀 I’m interested in Arduino, IoT, Robotica
 - 🌱 I’m currently learning ...
 - 💞️ I’m looking to collaborate on ...
 - 📫 How to reach me ...
+
+# Projeto
+
+KEBA8 é um projeto que visa dar um feedback visual para donos de plantinhas que querem saber quando regar sua planta. 
+
+# Componentes
+
+ - Arduino Mega  2560
+ - Jumpers
+ - Display LCD 20x4 com LCI
+ - Sensor de umidade do solo
+ - Protoboard mini
+ - Papel alumínio
+
+# Arquitetura
+
+ ![](img/arquitetura.png)
+
+ 
+ 
 
 <!---
 amarianno/amarianno is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
---
+++ sketch_completo/sketch_completo.ino
@@ -0,0 +1,118 @@
+//Programa: Monitoracao de planta usando Arduino
+//Autor: FILIPEFLOP
+
+//BRANCO VCC
+//CINZA GND
+//ROXO A0
+
+#include <Wire.h>;
+#include <LiquidCrystal_I2C.h>;
+
+LiquidCrystal_I2C lcd(0x27,20,4);
+ 
+#define pino_sinal_analogico A0
+
+int valor_analogico;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  //Inicializa o LCD e o backlight
+  lcd.init();
+  lcd.backlight();
+}
+ 
+void loop()
+{
+  //Le o valor do pino A0 do sensor
+  valor_analogico = analogRead(pino_sinal_analogico);
+ 
+  //Mostra o valor da porta analogica no serial monitor
+  Serial.print("Porta analogica: ");
+  Serial.print(valor_analogico);
+ 
+  //Solo umido, acende o led verde
+  if (valor_analogico > 0 && valor_analogico < 400)
+  {
+    limpar();
+    Serial.println(" Status: Solo umido");
+     lcd.setCursor(4, 1);
+     lcd.print(      "=) Solo umido"        );
+     lcd.setCursor(6, 2);
+     lcd.print(     valor_analogico        );   
+  }
+ 
+  //Solo com umidade moderada, acende led amarelo
+  if (valor_analogico > 400 && valor_analogico < 800)
+  {
+    limpar();
+    Serial.println(" Status: Umidade moderada");
+    lcd.setCursor(4, 1);
+     lcd.print(      "=| Solo moderado"        );
+     lcd.setCursor(6, 2);
+     lcd.print(      valor_analogico        );
+  }
+ 
+  //Solo seco, acende led vermelho
+  if (valor_analogico > 800 && valor_analogico < 1024)
+  {
+    limpar();
+    Serial.println(" Status: Solo seco");
+    lcd.setCursor(4, 1);
+     lcd.print(      "=( Solo SECO"        );
+     lcd.setCursor(6, 2);
+     lcd.print(    valor_analogico        );
+  }
+  
+  delay(7000);
+}
+
+void limpar() {
+  lcd.setCursor(0, 0);
+  lcd.print("                    ");
+  lcd.setCursor(0, 1);
+  lcd.print("                    ");
+  lcd.setCursor(0, 2);
+  lcd.print("                    ");
+  lcd.setCursor(0, 3);
+  lcd.print("                    ");
+}
+
+void sorriso1() {
+  lcd.setCursor(7, 1);
+  lcd.print(      "(#) (#)"        );
+  lcd.setCursor(1, 2);
+  lcd.print( "**              **"  );
+  lcd.setCursor(3, 3);
+  lcd.print(   "**************"    );
+
+  delay(7000);
+
+  lcd.setCursor(7, 1);
+  lcd.print(      "(-) (-)"        );
+  lcd.setCursor(1, 2);
+  lcd.print( "**              **"  );
+  lcd.setCursor(3, 3);
+  lcd.print(   "**************"    );
+
+  delay(200);
+
+  lcd.setCursor(7, 1);
+  lcd.print(      "(#) (#)"        );
+  lcd.setCursor(1, 2);
+  lcd.print( "**              **"  );
+  lcd.setCursor(3, 3);
+  lcd.print(   "**************"    );
+
+  delay(200);
+
+  lcd.setCursor(7, 1);
+  lcd.print(      "(-) (-)"        );
+  lcd.setCursor(1, 2);
+  lcd.print( "**              **"  );
+  lcd.setCursor(3, 3);
+  lcd.print(   "**************"    );
+
+  delay(200);
+}
